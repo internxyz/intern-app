f206935972d61bc5fcee5ae0315bb730db62c8c9
ZxStim <thang.victor.luong@gmail.com>
2025-03-29 02:43:19 +0700

    feat: added passkey

--- components/wallet-home.tsx
+++ components/wallet-home.tsx
@@ -7,13 +7,14 @@ import { Button } from "@/components/ui/button";
 import WalletUnlock from "@/components/wallet-unlock";
 import WalletOnboarding from "@/components/wallet-onboarding";
 
-export interface INTERN_WALLET_STATES {
-  IS_UNLOCKED: boolean;
-  WALLET_IDS: string[];
+export interface InternWalletState {
+  isUnlocked: boolean;
+  walletIds: string[];
+  currentAddress: string;
 }
 
 // Set the string key and the initial value
-export const internWalletStateAtom = atomWithStorage<INTERN_WALLET_STATES | undefined>('INTERN_WALLET_STATE', undefined)
+export const internWalletStateAtom = atomWithStorage<InternWalletState | undefined>('INTERN_WALLET_STATE', undefined)
 
 export default function WalletHome() {
   const [internWalletState, setInternWalletState] = useAtom(internWalletStateAtom)
@@ -21,14 +22,14 @@ export default function WalletHome() {
   return (
     <div className="flex flex-col gap-8">
       {
-        internWalletState && internWalletState.WALLET_IDS.length > 0 ? (
+        internWalletState && internWalletState.walletIds.length > 0 ? (
           <>
             <div className="flex flex-col gap-2">
               {
-                internWalletState.WALLET_IDS.map((walletId) => (
-                <div key={walletId}>{walletId}</div>
-              ))
-            }
+                internWalletState.walletIds.map((walletId) => (
+                  <div key={walletId}>{walletId}</div>
+                ))
+              }
           </div>
           <WalletUnlock />
           </>
@@ -36,6 +37,7 @@ export default function WalletHome() {
           <WalletOnboarding />
         )
       }
+      <WalletOnboarding />
       <Button onClick={() => setInternWalletState(RESET)}>
         Clear
       </Button>
--- components/wallet-onboarding.tsx
+++ components/wallet-onboarding.tsx
@@ -17,19 +17,65 @@ import {
   DrawerClose,
   DrawerContent,
   DrawerDescription,
-  DrawerFooter,
   DrawerHeader,
   DrawerTitle,
   DrawerTrigger,
 } from "@/components/ui/drawer"
-import { Lock, WalletMinimal, X } from "lucide-react"
+import { Lock, WalletMinimal, X, BadgePlus, Import, RotateCcw, Loader2, Check } from "lucide-react";
+import { useForm } from "@tanstack/react-form";
+import type { AnyFieldApi } from "@tanstack/react-form";
+import { useAtom } from "jotai";
+import { internWalletStateAtom } from "@/components/wallet-home";
+import { createOrThrow } from "@/lib/sigpass";
+
 
 export default function WalletOnboarding() {
   const [openFirst, setOpenFirst] = useState(false)
   const [openSecond, setOpenSecond] = useState(false)
   const [openThird, setOpenThird] = useState(false)
-
+  const [internWalletState, setInternWalletState] = useAtom(internWalletStateAtom)
   const isDesktop = useMediaQuery("(min-width: 768px)")
+
+  // form
+  const form = useForm({
+    defaultValues: {
+      walletName: "",
+    },
+    onSubmit: async ({ value }) => {
+      // Do something with form data
+      console.log(value)
+    },
+  })
+
+  async function createInternWallet(name: string) {
+    const bytes = crypto.getRandomValues(new Uint8Array(32));
+    /**
+     * Store the private key into authenticated storage
+     */
+    const handle = await createOrThrow(name, bytes);
+    /**
+     * Store the handle to the private key into some unauthenticated storage
+     */
+    if (!handle) {
+      return null;
+    }
+
+    if (internWalletState?.walletIds.length === 0) {
+      const newWalletId = `${name}/${handle.toString()}`
+      setInternWalletState({
+        walletIds: [...(internWalletState?.walletIds || []), newWalletId],
+        currentAddress: "",
+        isUnlocked: false,
+      })
+    } else {
+      const newWalletId = `${name}/${handle.toString()}`
+      setInternWalletState({
+        walletIds: [...(internWalletState?.walletIds || []), newWalletId],
+        currentAddress: "",
+        isUnlocked: false,
+      })
+    }
+  }
 
   // desktop
   if (isDesktop) {
@@ -78,18 +124,20 @@ export default function WalletOnboarding() {
                 </DrawerClose>
               </div>
             </DrawerTitle>
-            <DrawerDescription>
-              <div className="h-[4px] w-full rounded-full bg-gray-200 mt-4" />
-            </DrawerDescription>
+            <div className="h-[4px] w-full rounded-full bg-gray-200 mt-4" />
           </DrawerHeader>
-          <div className="flex flex-col gap-4 px-4 mt-2">
+          <div className="flex flex-col gap-4 px-4 pb-6 mt-2">
             <DrawerNested open={openSecond} onOpenChange={setOpenSecond}>
               <DrawerTrigger asChild>
-                <Button variant="outline">
-                  Create new
+                <Button className="flex flex-row gap-4 h-16 text-left justify-start items-start" variant="secondary">
+                  <BadgePlus className="mt-1" />
+                  <div className="flex flex-col gap-1">
+                    <div className="text-sm font-medium">Create new</div>
+                    <div className="text-xs text-muted-foreground">Create a fresh address with no previous history</div>
+                  </div>
                 </Button>
               </DrawerTrigger>
-              <DrawerContent className="h-[100vh]">
+              <DrawerContent>
                 <DrawerHeader>
                   <DrawerTitle>
                     <div className="flex flex-row items-center justify-between">
@@ -101,23 +149,84 @@ export default function WalletOnboarding() {
                       </DrawerClose>
                     </div>
                   </DrawerTitle>
-                  <DrawerDescription>
-                    <div className="h-[4px] w-full rounded-full bg-gray-200 mt-4" />
-                  </DrawerDescription>
+                  <div className="h-[4px] w-full rounded-full bg-gray-200 mt-4" />
                 </DrawerHeader>
-                <div className="flex flex-col gap-4 px-4 mt-2">
-                  <input 
-                    type="text" 
-                    placeholder="Type a name for your wallet" 
-                    className="w-full text-xl outline-none"
-                  />
+                <div className="flex flex-col gap-4 px-4 pb-6 mt-2">
+                  <form
+                    onSubmit={(e) => {
+                      e.preventDefault()
+                      e.stopPropagation()
+                      form.handleSubmit()
+                    }}
+                  >
+                    <div>
+                      {/* A type-safe field component*/}
+                      <form.Field
+                        name="walletName"
+                        validators={{
+                          onChange: ({ value }) =>
+                            !value
+                              ? '...waiting for a name'
+                              : value.length < 3
+                                ? 'Wallet name must be at least 3 characters'
+                                : undefined,
+                          onChangeAsyncDebounceMs: 100,
+                          onChangeAsync: async ({ value }) => {
+                            return (
+                              value.includes('error') && 'No "error" allowed in name'
+                            )
+                          },
+                        }}
+                      >
+                        {(field) => (
+                          <div className="flex flex-col gap-1 h-16">
+                            <input
+                              id={field.name}
+                              name={field.name}
+                              value={field.state.value}
+                              onBlur={field.handleBlur}
+                              onChange={(e) => field.handleChange(e.target.value)}
+                              placeholder="Type a name for your wallet"
+                              className="w-full text-2xl outline-none"
+                            />
+                            <FieldInfo field={field} />
+                          </div>
+                        )}
+                      </form.Field>
+                    </div>
+                    <form.Subscribe
+                      selector={(state) => [state.canSubmit, state.isSubmitting]}
+                    >
+                      {([canSubmit, isSubmitting]) => (
+                        <div className="flex flex-row gap-2 mt-4 justify-end">
+                          <Button size="icon" variant="secondary" type="reset" onClick={() => form.reset()}>
+                            <RotateCcw />
+                          </Button>
+                          <Button size="lg" type="submit" disabled={!canSubmit}>
+                            {isSubmitting ? 
+                              <Loader2 className="animate-spin" />
+                              : 
+                              <>
+                                <BadgePlus />
+                                Create
+                              </>
+                            }
+                          </Button>
+                        </div>
+                      )}
+                    </form.Subscribe>
+                  </form>
                 </div>
               </DrawerContent>
             </DrawerNested>
             <DrawerNested open={openThird} onOpenChange={setOpenThird}>
               <DrawerTrigger asChild>
-                <Button variant="outline">
-                  Add existing
+                <Button className="flex flex-row gap-4 h-16 text-left justify-start items-start" variant="secondary">
+                  <Import className="mt-1" />
+                  <div className="flex flex-col gap-1">
+                    <div className="text-sm font-medium">Add existing</div>
+                    <div className="text-xs text-muted-foreground">Add an existing wallet by importing or restoring</div>
+                  </div>
                 </Button>
               </DrawerTrigger>
               <DrawerContent className="h-[100vh]">
@@ -139,11 +248,24 @@ export default function WalletOnboarding() {
               </DrawerContent>
             </DrawerNested>
           </div>
-          <DrawerFooter className="pt-2">
-            Learn more
-          </DrawerFooter>
         </DrawerContent>
       </Drawer>
+    </div>
+  )
+}
+
+function FieldInfo({ field }: { field: AnyFieldApi }) {
+  return (
+    <div className="text-sm">
+      {field.state.meta.isValidating ? (
+        'Checking...'
+      ) : field.state.meta.isTouched ? (
+        field.state.meta.errors.length ? (
+          <em>{field.state.meta.errors.join(',')}</em>
+        ) : (
+          <div className="text-green-600 flex flex-row gap-2 items-center"><Check className="w-5 h-5" />perfect</div>
+        )
+      ) : null}
     </div>
   )
 }
